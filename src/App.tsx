f06e079ee8312ca595a096891c3bc2e10e13d08b
jespinoza <jespinoza@silta.cl>
2023-09-24 17:35:30 -0300

    Add doc

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import "./App.css";
 import { store } from "./config/store";
 import { Provider } from "react-redux";
 import ApplicationBody from "./components/ApplicationBody/ApplicationBody";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { ThemeProvider } from "@mui/material/styles";
 import { themeOptions } from "./theme/theme";
 
+/**
+ * Full application. It's wrapped in the provider component
+ * to give access to the state and the ThemeProvider to grant
+ * access to the theme through the app.
+ */
 const App = () => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={themeOptions}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <ApplicationBody />
-        </LocalizationProvider>
+        <ApplicationBody />
       </ThemeProvider>
     </Provider>
   );
