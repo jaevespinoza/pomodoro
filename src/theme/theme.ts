a20c449f29a3d539dcf7cea80a5deeb32604d68a
jespinoza <jespinoza@silta.cl>
2023-09-24 17:33:56 -0300

    Add more documentation

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,5 +1,10 @@
 import { createTheme } from "@mui/material/styles";
 
+/**
+ * We add more variables to the Theme interface to help us with the
+ * definition of theme variables. For example, we define the workTimer, shortBreak,
+ * and longBreak objects that will hold the color associated to it and its button.
+ */
 declare module "@mui/material/styles" {
   interface Theme {
     workTimer: {
@@ -32,6 +37,13 @@ declare module "@mui/material/styles" {
   }
 }
 
+/**
+ * Theme of the application. We define the various colors that we will use
+ * across the app, centralizing the information for easier access.
+ * This means that, if we want to change the color, all we need to do
+ * is change the color in this file and it will spread to all the parts
+ * of the application that use it.
+ */
 export const themeOptions = createTheme({
   palette: {
     mode: "light",
